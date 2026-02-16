a7d844cdd64d724518699940147d1c5a923cef44
Arsalan Rabdanov <arsalan.rabdanov2078@gmail.com>
2026-02-16 10:55:27 -0500

    starting scss added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
-import './App.css'
+import "./App.css";
 
 export default function App() {
   return (
-    <div>App</div>
-  )
+    <>
+      <div className="text-preset-1">The quick brown fox jumps over the lazy dog.</div>
+      <div className="text-preset-2">The quick brown fox jumps over the lazy dog.</div>
+      <div className="text-preset-3">The quick brown fox jumps over the lazy dog.</div>
+      <div className="text-preset-4">The quick brown fox jumps over the lazy dog.</div>
+    </>
+  );
 }
